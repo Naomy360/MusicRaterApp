66804a6673175ecde03df35222ffd689cd4b1e4a
Naomy360 <85355678+Naomy360@users.noreply.github.com>
2023-11-13 16:40:34 -0500

    RatingModel.php

---
+++ MusicRaterApp/Models/RatingModel.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../Config/Database.php';
+
+class RatingModel {
+    private $db;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->connect();
+    }
+
+    public function addRating($username, $artist, $song, $rating) {
+        $stmt = $this->db->prepare("INSERT INTO ratings_table (username, artist, song, rating) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$username, $artist, $song, $rating]);
+        return $stmt->rowCount();
+    }
+
+    public function getAllRatings() {
+        $stmt = $this->db->query("SELECT * FROM ratings_table");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateRating($id, $artist, $song, $rating) {
+        $stmt = $this->db->prepare("UPDATE ratings_table SET artist = ?, song = ?, rating = ? WHERE id = ?");
+        $stmt->execute([$artist, $song, $rating, $id]);
+        return $stmt->rowCount();
+    }
+    
+
+    public function deleteRating($id) {
+        $stmt = $this->db->prepare("DELETE FROM ratings_table WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->rowCount();
+    }
+}
+
+
