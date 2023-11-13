8c1f9006300b2ede0d2a55173342e189cebfffcb
Naomy360 <85355678+Naomy360@users.noreply.github.com>
2023-11-13 17:01:49 -0500

    modified index.php

--- MusicRaterApp/Public/Index.php
+++ MusicRaterApp/Public/Index.php
@@ -1,30 +1,63 @@
 <?php
-require_once '../controllers/UserController.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once '../Controllers/UserController.php';
+require_once '../Controllers/RatingController.php'; // Include the RatingController
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE"); // Allow additional methods
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
 $userController = new UserController();
+$ratingController = new RatingController(); // Instantiate the RatingController
 
 $data = json_decode(file_get_contents("php://input"));
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Determine if the action is to sign up or to log in
-    if (isset($data->action) && $data->action === 'signup') {
-        $success = $userController->signUp($data->username, $data->password);
-        $message = $success ? 'User created successfully' : 'User could not be created';
-    } elseif (isset($data->action) && $data->action === 'login') {
-        $success = $userController->logIn($data->username, $data->password);
-        $message = $success ? 'Login successful' : 'Invalid username or password';
+    if (isset($data->action)) {
+        switch ($data->action) {
+            case 'signup':
+                $success = $userController->signUp($data->username, $data->password);
+                $message = $success ? 'User created successfully' : 'User could not be created';
+                break;
+            case 'login':
+                $success = $userController->logIn($data->username, $data->password);
+                $message = $success ? 'Login successful' : 'Invalid username or password';
+                break;
+            case 'createRating':
+                $result = $ratingController->createRating($data->username, $data->artist, $data->song, $data->rating);
+                $success = $result > 0;
+                $message = $success ? 'Rating added successfully' : 'Failed to add rating';
+                break;
+            default:
+                $success = false;
+                $message = 'No action specified or action is invalid.';
+                break;
+        }
+        echo json_encode(['success' => $success, 'message' => $message]);
     } else {
-        $success = false;
-        $message = 'No action specified or action is invalid.';
+        echo json_encode(['message' => 'Action not specified']);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo json_encode($ratingController->getRatings());
+} elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    if (isset($data->id) && isset($data->artist) && isset($data->song) && isset($data->rating)) {
+        $result = $ratingController->updateRating($data->id, $data->artist, $data->song, $data->rating);
+        echo json_encode(['success' => $result > 0, 'message' => $result > 0 ? 'Rating updated successfully' : 'Failed to update rating']);
+    } else {
+        echo json_encode(['message' => 'Missing data for updating rating']);
     }
 
-    echo json_encode(array('success' => $success, 'message' => $message));
+} elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    if (isset($data->id)) {
+        $result = $ratingController->deleteRating($data->id);
+        echo json_encode(['success' => $result > 0, 'message' => $result > 0 ? 'Rating deleted successfully' : 'Failed to delete rating']);
+    } else {
+        echo json_encode(['message' => 'Missing ID for deleting rating']);
+    }
 } else {
-    echo json_encode(array('message' => 'Only POST requests are allowed'));
+    echo json_encode(['message' => 'Method not allowed']);
 }
-
