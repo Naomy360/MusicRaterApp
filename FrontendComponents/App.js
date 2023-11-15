4ab8e90eae0e150c801d866c0dfd7d382f836601
Naomy360 <85355678+Naomy360@users.noreply.github.com>
2023-11-15 06:25:43 -0500

    Update App.js

--- FrontendComponents/App.js
+++ FrontendComponents/App.js
@@ -1,50 +1,46 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './Login'; 
-import SignUp from './SignUp'; 
-import Index from './Index'; 
+
+
+import Login from './Login';
+import SignUp from './SignUp';
+import Index from './Index';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-          name="Login" 
-          component={Login} 
-          options={{ title: 'Login' }} 
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUp} 
-          options={{ title: 'Sign Up' }} 
-        />
-        <Stack.Screen 
-          name="Index" 
-          component={Index} 
-          options={{ title: 'Welcome' }} // Title for the welcome screen
-        />
-        {/* You can add more screens to the navigator here */}
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }} />
+          <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Sign Up' }} />
+          <Stack.Screen name="Index" component={Index} options={{ title: 'FaveTune' }} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+
+    
+
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-
-
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
