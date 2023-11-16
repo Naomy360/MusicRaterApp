73b0f4e0cfffd6c7d12a4f4f3c6943a22f70aa4a
Jmac041 <144952777+Jmac041@users.noreply.github.com>
2023-11-15 22:55:51 -0500

    Update RatingController.php

--- MusicRaterApp/Controllers/RatingController.php
+++ MusicRaterApp/Controllers/RatingController.php
@@ -1,34 +1,44 @@
 <?php
+
+// Include the RatingModel
 require_once '../Models/RatingModel.php';
 
 class RatingController {
+    // RatingModel instance
     private $model;
 
+    // initialize the RatingModel
     public function __construct() {
+        // Instantiate the RatingModel
         $this->model = new RatingModel();
     }
 
+    // Handle the creation of a new rating
     public function createRating($username, $artist, $song, $rating) {
+        // Check if the user has already rated the song
         if ($this->model->ratingExists($username, $artist, $song)) {
             return ['success' => false, 'message' => 'You have already rated this song'];
         }
-        // Insert rating logic remains the same
+
+        // Call the addRating method of the RatingModel to add a new rating
         return ['success' => true, 'message' => 'Rating added successfully', 'data' => $this->model->addRating($username, $artist, $song, $rating)];
     }
-    
-    
 
+    // retrieve all ratings
     public function getRatings() {
+        // Call the getAllRatings method of the RatingModel
         return $this->model->getAllRatings();
     }
+
+    // update an existing rating
     public function updateRating($id, $artist, $song, $rating) {
+        // Call the updateRating method of the RatingModel
         return $this->model->updateRating($id, $artist, $song, $rating);
     }
-    
 
+    // Delete an existing rating
     public function deleteRating($id) {
+        // Call the deleteRating method of the RatingModel
         return $this->model->deleteRating($id);
     }
 }
-
-
